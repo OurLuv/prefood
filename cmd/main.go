c4a898e6ba71718084dac2cc52fab8810300d498
ourluv <aorlowde@gmail.com>
2023-09-10 21:28:57 +0300

    added config

--- cmd/main.go
+++ cmd/main.go
@@ -3,20 +3,29 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	"github.com/OurLuv/prefood/internal/config"
 	"github.com/joho/godotenv"
+	"golang.org/x/exp/slog"
 )
 
 // init is invoked before main()
-func init() {
-    if err := godotenv.Load(); err != nil {
-        log.Print("No .env file found")
-    }
+func initEnv() {
+	if err := godotenv.Load(); err != nil {
+		log.Print("No .env file found")
+	}
 }
 
 func main() {
+	initEnv()
 	cfg := config.MustLoad()
-
 	fmt.Println(cfg)
+	log := setupLogger()
+	log.Info("Test info")
+}
+
+func setupLogger() *slog.Logger {
+	log := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	return log
 }
--- internal/config/config.go
+++ internal/config/config.go
@@ -11,25 +11,25 @@ import (
 type Config struct {
 	Env         string `yaml:"env" env-default:"local"`
 	StoragePath string `yaml:"storage_path"`
-	HttpServer `yaml:"http_server"`
+	HttpServer  `yaml:"http_server"`
 }
 
 type HttpServer struct {
-	Address string `yaml:"address"`
-	Timeout time.Duration `yaml:"timeout"`
+	Address     string        `yaml:"address"`
+	Timeout     time.Duration `yaml:"timeout"`
 	IdleTimeout time.Duration `yaml:"idle_timeout"`
 }
 
-func MustLoad() *Config{
+func MustLoad() *Config {
 	configPath, exists := os.LookupEnv("CONFIG_PATH")
 
-    if !exists {
-    	log.Fatalf("CONFIG PATH is not set: %s", configPath)
-   }
+	if !exists {
+		log.Fatalf("CONFIG PATH is not set: %s", configPath)
+	}
 
-   var cfg Config
+	var cfg Config
 
-   err := cleanenv.ReadConfig(configPath, &cfg)
+	err := cleanenv.ReadConfig(configPath, &cfg)
 	if err != nil {
 		log.Fatalf("cannot read config: %s", err)
 	}
